c5d2c56ac3b1a3d87341673fa33ebbbc316968c0
João Gonçalves <joao_i0807@outlook.com>
2023-04-09 09:42:09 -0300

    feat pasting string methods to project

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 print("hello word!")
+upper = 'converte todas as letras de uma string para maiúsculas.'
+
+lower = 'converte todas as letras de uma string para minúsculas.'
+
+capitalize = 'converte a primeira letra de uma string para maiúscula e as demais para minúsculas.'
+
+title = 'converte a primeira letra de cada palavra de uma string para maiúscula e as demais para minúsculas.'
+
+strip = 'remove espaços em branco do início e do final de uma string.'
+
+replace = 'substitui uma parte de uma string por outra.'
+
+split = 'divide uma string em uma lista de strings, usando um determinado delimitador.'
+
+join ='junta uma lista de strings em uma única string, usando um determinado delimitador.'
+
+find = 'encontra a primeira ocorrência de uma substring em uma string e retorna o índice em que ela se encontra.'
+
+count = 'conta quantas vezes uma substring aparece em uma string.'
